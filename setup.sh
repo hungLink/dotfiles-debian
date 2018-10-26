64add56512eb9a0c4e76f4f8748ba441a1fd19d1
Albert Lopez <administrationator@LittleDebian.parkhouse2.com>
2018-10-25 17:34:45 -0700

    completed the script

---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+####----- HARDLINK CONFIG FILES -----####
+files=$(find home -name "*")
+for file in $files; do
+	#if its a dir skip it
+	if [[ -d $file ]]; then
+    		continue	
+
+	elif [[ ! -f $file ]]; then
+    		echo "ERROR! $file isn't valid for some reason!"
+	
+		continue
+	fi
+
+	#We have a file on our hands
+	#Deterimine the path where the config file should go
+	link=$(echo $file | sed "s/home/~/")
+	#replace the ~ with the home path for the user
+	link="${link/#\~/$HOME}"
+
+	#Is the path clear to make a hard link? IE is there no file where we need the link?
+	if [[ ! -f $link ]]; then
+		ln $file $link
+		continue
+	fi
+
+	#There is a file where the link is trying to go.
+	#Are they hardlinked?
+	fileInode=$( ls -i $file | cut -f1 -d' ' )
+	linkInode=$( ls -i $link | cut -f1 -d' ' )
+	if [ $fileInode = $linkInode ]; then
+		#if they are harldinked already, then no action is neccessary.
+		continue	
+	fi
+	
+	#The file in the way is not a hard link
+	#Is the text identical?
+	if [[ ! $(comm -3 $file $link) ]]; then
+		rm $link
+		ln $file $link
+	fi
+	
+	echo "Processing "$file
+	echo "It looks like we have a file in the way with different contents then that of the one in the repo."
+	echo "I shall mv the existing one to "$link".sysConBak and make the link in the apporpriate place."
+	mv $link $link.sysConBak
+	ln $file $link
+	
+done
+
+####----- BASH AND SCRIPTS FOLDER -----####
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+appendToBashRC="if [ -f $DIR/.bash_aliases ]; then . $DIR/.bash_aliases; fi; if [ -f $DIR/.bash_functions ]; then . $DIR/.bash_functions; fi;"
+grepAppend=$(echo $appendToBashRC | sed "s/\[/\\\[/g" | sed "s/\]/\\\]/g")
+if [[ $(grep "$grepAppend" ~/.bashrc) ]]; then
+	echo "Bash custom has already be setup. try running 'source ~/.bashrc' or opening an new terminal session.";
+else
+	echo -e "" >> ~/.bashrc
+	comment="##Bash Custom##"
+	echo -e "Appending the following to your ~/.bashrc file"
+	echo -e $comment
+	echo -e $appendToBashRC
+	echo -e $comment >> ~/.bashrc
+	echo -e $appendToBashRC >> ~/.bashrc
+	echo -e "" >> ~/.bashrc
+fi;
+
+appendToBashRC='PATH="$PATH:$HOME/scripts"'
+grepAppend=$(echo $appendToBashRC | sed "s/\[/\\\[/g" | sed "s/\]/\\\]/g")
+if [[ $(grep "$grepAppend" ~/.bashrc) ]]; then
+	echo "The PATH already contains your scripts"
+else
+	comment="##Add scripts folder to PATH##"
+	echo -e "Appending the following to your ~/.bashrc file"
+	echo -e $comment
+	echo -e $appendToBashRC
+	echo -e $comment >> ~/.bashrc
+	echo -e $appendToBashRC >> ~/.bashrc
+	echo -e "" >> ~/.bashrc
+fi;
+source ~/.bashrc
+
+
