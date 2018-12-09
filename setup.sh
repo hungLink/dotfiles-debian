df37494606e59191ff7c3c9cb04851f24ca02046
Albert Lopez <albert.m.lopez@protonmail.com>
2018-12-09 11:52:56 -0800

    setup now creates dirs more reliably and handles spaces in dirnames/filenames

--- setup.sh
+++ setup.sh
@@ -1,39 +1,23 @@
 #!/bin/bash
 
 ####----- HARDLINK CONFIG FILES -----####
-files=$(find home -name "*")
-for file in $files; do
+find home -type f -name '*' -print0 | 
+while IFS= read -r -d '' file; do
 	#Deterimine the path where the config file should go
 	link=$HOME$(echo $file | sed "s/home//")
-
-	#if its a dir create it
-	if [[ -d $file ]]; then
-		#if the dir already exists, do nothing. Else Create it.
-		if [[ -d $link ]]; then
-			continue
-		fi
-		mkdir $link
-		continue
-
-	elif [[ ! -f $file ]]; then
-    		echo "ERROR! $file isn't valid for some reason!"
-		continue
-	fi
-
-	#We have a file on our hands
-	#replace the ~ with the home path for the user
-	link="${link/#\~/$HOME}"
+	dir=$(dirname "$link")
+	mkdir -p "$dir"
 
 	#Is the path clear to make a hard link? IE is there no file where we need the link?
 	if [[ ! -f $link ]]; then
-		ln $file $link
+		ln "$file" "$link"
 		continue
 	fi
 
 	#There is a file where the link is trying to go.
 	#Are they hardlinked?
-	fileInode=$( ls -i $file | cut -f1 -d' ' )
-	linkInode=$( ls -i $link | cut -f1 -d' ' )
+	fileInode=$( ls -i "$file" | cut -f1 -d' ' )
+	linkInode=$( ls -i "$link" | cut -f1 -d' ' )
 	if [ $fileInode = $linkInode ]; then
 		#if they are harldinked already, then no action is neccessary.
 		continue	
@@ -42,15 +26,15 @@ for file in $files; do
 	#The file in the way is not a hard link
 	#Is the text identical?
 	if [[ ! $(comm -3 $file $link) ]]; then
-		rm $link
-		ln $file $link
+		rm "$link"
+		ln "$file" "$link"
 	fi
 	
 	echo "Processing "$file
 	echo "It looks like we have a file in the way with different contents then that of the one in the repo."
-	echo "I shall mv the existing one to "$link".sysConBak and make the link in the apporpriate place."
-	mv $link $link.sysConBak
-	ln $file $link
+	echo "I shall mv the existing one to "$link".sysConBak and make the link in the appropriate place."
+	mv "$link" "$link".sysConBak
+	ln "$file" "$link"
 	
 done
 
