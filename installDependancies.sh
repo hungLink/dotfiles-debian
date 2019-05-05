539f41976a2c9769daff50dc225b7bad999dcfd3
gunrugger <gun.rugger@gmail.com>
2019-05-05 00:11:39 -0700

    added zathura, pass, and gimp to apt install command

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Going super saiyan. I won't let you get away with this."
-sudo apt install -y vim vim-gtk ranger i3-wm
+sudo apt install -y vim vim-gtk ranger i3-wm zathura pass gimp
 sudo apt install -y build-essential cmake python3-dev
 python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer
 vim +PluginInstall +qall
