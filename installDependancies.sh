22218656c6dad0ec5337557c20b0c2831695f7ba
gunrugger <gun.rugger@gmail.com>
2019-09-19 09:14:54 -0700

    added ipv6 and rollstats scripts

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packages = build-essential cmake djmount gimp htop i3-wm pass python3-dev ranger secure-delete tmux vim vim-gtk zathura
+packages = build-essential cmake djmount gimp htop i3-wm pass python3-dev ranger secure-delete tmux vim vim-gtk zathura python3-tk
 echo "Going super saiyan. I won't let you get away with this."
 sudo apt install -y $packages
 python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer
