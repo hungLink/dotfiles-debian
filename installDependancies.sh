af431fbf156561eaac8b6eb7cffc2fdeda9b8da4
gunrugger <gun.rugger@gmail.com>
2019-10-04 19:45:54 -0700

    added pywal

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packagesString="xorg kitty wget build-essential cmake djmount gimp htop i3-wm i3lock i3status bash- completion pass python3-dev ranger secure-delete tmux vim vim-gtk zathura python3-tk"
+packagesString="xorg python3-pip kitty wget build-essential cmake djmount gimp htop i3-wm i3lock i3status bash- completion pass python3-dev ranger secure-delete tmux vim vim-gtk zathura python3-tk"
 packagesArray=(${packagesString})
 
 echo "Going super saiyan. I won't let you get away with this."
@@ -7,5 +7,6 @@ for package in "${packagesArray[@]}"
 	do
 	sudo apt install -y $package
 	done
+sudo pip3 install pywal
 python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer
 vim +PluginInstall +qall
