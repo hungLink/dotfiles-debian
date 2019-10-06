c736bfef0a8f0bb55c0b690a7f9db0e38e2f477e
gunrugger <gun.rugger@gmail.com>
2019-10-05 23:07:43 -0700

    organized packages alphabetically, added zsh, removed kitty, fixed typo 'bash- completion' to 'bash-completion'

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packagesString="xorg python3-pip kitty wget build-essential cmake djmount gimp htop i3-wm i3lock i3status bash- completion pass python3-dev ranger secure-delete tmux vim vim-gtk zathura python3-tk"
+packagesString="bash-completion build-essential cmake djmount gimp htop i3-wm i3lock i3status pass python3-dev python3-pip python3-tk ranger secure-delete tmux vim vim-gtk wget xorg zathura zsh zsh-syntax-highlighting"
 packagesArray=(${packagesString})
 
 echo "Going super saiyan. I won't let you get away with this."
