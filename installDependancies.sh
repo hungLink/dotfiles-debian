313e00c2c9fd6ff6ad0c59378665d0e0badb732a
gunrugger <gun.rugger@gmail.com>
2019-10-13 10:36:45 -0700

    added compton config for transparent windows. Don't use tabs in i3.

--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packagesString="bash-completion build-essential cmake djmount gimp htop i3-wm i3lock i3status pass python3-dev python3-pip python3-tk ranger secure-delete tmux vim vim-gtk wget xorg zathura zsh zsh-syntax-highlighting"
+packagesString="bash-completion build-essential cmake compton djmount feh gimp htop i3-wm i3lock i3status pass python3-dev python3-pip python3-tk ranger secure-delete tmux vim vim-gtk wget xorg zathura zsh zsh-syntax-highlighting"
 packagesArray=(${packagesString})
 
 echo "Going super saiyan. I won't let you get away with this."
