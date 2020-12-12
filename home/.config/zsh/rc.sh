31adcd1ee6a81a1718ebfa168009a656e8743ad9
gunrugger <gun.rugger@gmail.com>
2020-12-12 15:27:04 -0800

    I don't know what I changed. Living circumstances changed, and these changes are mostly like a year old.

--- home/.config/zsh/rc.sh
+++ home/.config/zsh/rc.sh
@@ -33,3 +33,6 @@ bindkey '^E' edit-command-line
 bindkey -s '^o' 'rangercd\n'
 
 
+#load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
--- installDependancies.sh
+++ installDependancies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packagesString="bash-completion build-essential cmake compton djmount feh gimp htop i3-wm i3lock i3status pass python3-dev python3-pip python3-tk ranger secure-delete tmux vim vim-gtk wget xorg zathura zsh zsh-syntax-highlighting"
+packagesString="bash-completion build-essential cmake compton djmount feh gimp htop i3-wm i3lock i3status pass python3-dev python3-pip python3-tk ranger secure-delete tmux vim vim-gtk wget xorg xsel zathura zsh zsh-syntax-highlighting"
 packagesArray=(${packagesString})
 
 echo "Going super saiyan. I won't let you get away with this."
@@ -7,6 +7,3 @@ for package in "${packagesArray[@]}"
 	do
 	sudo apt install -y $package
 	done
-sudo pip3 install pywal
-python3 ~/.vim/bundle/YouCompleteMe/install.py --all --clang-completer
-vim +PluginInstall +qall
