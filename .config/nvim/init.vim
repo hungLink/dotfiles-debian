1b07b745d2c770727914369ce3efb5c2598ea040
Albert Lopez <albertlopez@pop-os.localdomain>
2022-11-21 13:42:11 -0800

    enable packer to install when launching nvim

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -16,3 +16,9 @@ let g:godot_executable = '/Applications/Godot.app'
 " disable providers
 let g:loaded_perl_provider = 0
 let g:loaded_ruby_provider = 0
+
+" recompile packer upon changing it's config file
+augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+augroup end
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,16 @@
--- "opt": plugin is optional - https://github.com/wbthomason/packer.nvim#specifying-plugins
---
+-- Install packer automatically for the sake of new setups
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- package manager
